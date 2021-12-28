e78c6b16a3b1bc33b6227880d314d7ec409eca5f
Jeniffer Callou <jenifferprc@gmail.com>
2021-12-28 22:07:58 +0100

    improved + comments

--- 01_multiples_of_3_or_5/01.rb
+++ 01_multiples_of_3_or_5/01.rb
@@ -3,12 +3,13 @@
 # 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-numbers = (1...1000).to_a
-multiples = []
-numbers.each do |n|
-    if n % 3 == 0 || n % 5 == 0
-        multiples << n
+# Solution:
+multiples = []                  # empty array to save the multiples;
+
+(1...1000).to_a.each do |n|     # loop through the array of the range 1 to 999;
+    if n % 3 == 0 || n % 5 == 0 # condition to find the multiples;
+        multiples << n          # saving the multiples;
     end
 end
-p multiples.sum
-p multiples.count
+puts "Result: #{multiples.sum}" # sum the multiples direct on print statement;
+

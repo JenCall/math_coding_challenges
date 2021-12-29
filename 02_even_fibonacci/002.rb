fed69195864a6ed0b07f3fa48c7dda83ccf9fe84
Jeniffer Callou <jenifferprc@gmail.com>
2021-12-29 21:23:10 +0100

    added my solution

--- 02_even_fibonacci/002.rb
+++ 02_even_fibonacci/002.rb
@@ -3,7 +3,6 @@
 # do not exceed four million, find the sum of the even-valued terms.
 
 # 01-mySolution
-
 fibonacci_sequence = [1, 2]
 even_sum = []
 i = 1
@@ -12,7 +11,6 @@ while fibonacci_sequence[-1] < 4000000
     fibonacci_sequence << fibonacci_sequence[i-1] + fibonacci_sequence[i]
     i += 1
 end
-
 
 fibonacci_sequence.each { |n| even_sum << n if n % 2 == 0 }
 puts even_sum.sum

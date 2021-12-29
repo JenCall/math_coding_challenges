10f1a7f7063711a61fe01f58899bb4ddbe3a9951
Jeniffer Callou <jenifferprc@gmail.com>
2021-12-29 20:46:15 +0100

    added  cooments

--- 01_multiples_of_3_or_5/01.rb
+++ 01_multiples_of_3_or_5/01.rb
@@ -3,7 +3,9 @@
 # 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-# my Solution:
+# 01-mySolution:
+# starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+# code:
 multiples = []                  # empty array to save the multiples;
 
 (1...1000).to_a.each do |n|     # loop through the array of the range 1 to 999;
@@ -13,10 +15,17 @@ multiples = []                  # empty array to save the multiples;
 end
 puts "Result: #{multiples.sum}" # sum the multiples direct on print statement;
 
-# suggested solution - more efficient solution
+# ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+# p elapsed = ending - starting     # 0.0001 seconds
 
-def sumDivisibleBy(target, n)
-    p = target / n
-    n * (p * (p + 1 )) / 2
+# 02-suggestedSolution - more efficient solution
+# starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+# code:
+def sumDivisibleBy(target, n)   # function that takes the target and the number
+    p = target / n              # math solution div target per n
+    n * (p * (p + 1 )) / 2      # sum 1 then multiple by p then by n then div per 2
 end
 puts sumDivisibleBy(999, 3) + sumDivisibleBy(999, 5) - sumDivisibleBy(999, 15)
+
+# ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+# p elapsed = ending - starting   # 6.99 seconds

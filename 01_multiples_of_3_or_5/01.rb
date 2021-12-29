1b42fd95422c568dfd04428673154275a36cc5a0
Jeniffer Callou <jenifferprc@gmail.com>
2021-12-29 20:31:54 +0100

    added more efficient solution

--- 01_multiples_of_3_or_5/01.rb
+++ 01_multiples_of_3_or_5/01.rb
@@ -3,7 +3,7 @@
 # 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-# Solution:
+# my Solution:
 multiples = []                  # empty array to save the multiples;
 
 (1...1000).to_a.each do |n|     # loop through the array of the range 1 to 999;
@@ -13,3 +13,10 @@ multiples = []                  # empty array to save the multiples;
 end
 puts "Result: #{multiples.sum}" # sum the multiples direct on print statement;
 
+# suggested solution - more efficient solution
+
+def sumDivisibleBy(target, n)
+    p = target / n
+    n * (p * (p + 1 )) / 2
+end
+puts sumDivisibleBy(999, 3) + sumDivisibleBy(999, 5) - sumDivisibleBy(999, 15)

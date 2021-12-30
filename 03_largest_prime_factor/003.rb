2cc9e9ba949e06a81cc898c4c731385e94a17d7d
Jeniffer Callou <jenifferprc@gmail.com>
2021-12-30 22:59:41 +0100

    added my solution

--- 03_largest_prime_factor/003.rb
+++ 03_largest_prime_factor/003.rb
@@ -3,8 +3,21 @@
 # What is the largest prime factor of the number 600851475143?
 
 require 'prime'
+number = 13195
+
 prime = []
-Prime.each(100) do |n|
+Prime.each(number) do |n|
     prime << n  #=> 2, 3, 5, 7, 11, ...., 97
 end
-p prime
+
+i = 0
+prime_num = []
+
+while prime[i] < number
+    n = prime[i]
+    if (number % n == 0)
+        prime_num << n
+        p prime_num[-1]
+    end
+    i += 1
+end

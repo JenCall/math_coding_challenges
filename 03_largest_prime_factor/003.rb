c7cee82fa2beb1f6d262dbca3562f4c000d5eed4
Jeniffer Callou <jenifferprc@gmail.com>
2021-12-30 22:35:19 +0100

    added my solution

--- 03_largest_prime_factor/003.rb
+++ 03_largest_prime_factor/003.rb
@@ -2,8 +2,9 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143?
 
+require 'prime'
 prime = []
-
-(1..1000).each do |n|
-    if n % 
+Prime.each(100) do |n|
+    prime << n  #=> 2, 3, 5, 7, 11, ...., 97
 end
+p prime

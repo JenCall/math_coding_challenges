980f0abf1f8bcb78f3e95c6a825451e768a7d359
Jeniffer Callou <jenifferprc@gmail.com>
2021-12-31 08:33:58 +0100

    added my solution

--- 03_largest_prime_factor/003.rb
+++ 03_largest_prime_factor/003.rb
@@ -1,13 +1,11 @@
-# Problem 03 - Largest Prime Factor
-# The prime factors of 13195 are 5, 7, 13 and 29.
-# What is the largest prime factor of the number 600851475143?
+# Problem 03 - What is the largest prime factor?
 
 require 'prime'
 
 # 01 - My Solution
 def largest_prime(number)
     prime_factors = []
-    Prime.each(10000) do |prime|    # if for 5 digits prime factor of 2 digits, so for 12 -> 5
+    Prime.each(10000) do |prime|    # if 5 dgs -> pf <= 2dgs, so 12dgs -> <= 5dgs
         if prime * prime <= number
             if (number % prime == 0)
                 prime_factors << prime 
@@ -16,10 +14,10 @@ def largest_prime(number)
     end
     p prime_factors[-1]
 end
-largest_prime(600851475143)
+largest_prime(7000000)
 
 # 02 - optimized Solution in Ruby methods
 def lpf(n)
     puts Prime.prime_division(n).last[0]
 end
-lpf(600851475143)
+lpf(7000000)

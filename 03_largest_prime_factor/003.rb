f6304a8760442c94c3588a6410dba5ae674022a5
Jeniffer Callou <jenifferprc@gmail.com>
2021-12-31 08:24:59 +0100

    added my solution

--- 03_largest_prime_factor/003.rb
+++ 03_largest_prime_factor/003.rb
@@ -3,12 +3,11 @@
 # What is the largest prime factor of the number 600851475143?
 
 require 'prime'
-# number = 600851475143
 
-# 01- my solution for not high numbers  
+# 01 - My Solution
 def largest_prime(number)
     prime_factors = []
-    Prime.each(number) do |prime|
+    Prime.each(10000) do |prime|    # if for 5 digits prime factor of 2 digits, so for 12 -> 5
         if prime * prime <= number
             if (number % prime == 0)
                 prime_factors << prime 
@@ -17,5 +16,10 @@ def largest_prime(number)
     end
     p prime_factors[-1]
 end
+largest_prime(600851475143)
 
-largest_prime(13195)
+# 02 - optimized Solution in Ruby methods
+def lpf(n)
+    puts Prime.prime_division(n).last[0]
+end
+lpf(600851475143)

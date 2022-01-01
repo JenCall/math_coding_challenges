bb6ee9f16e3fe6a9e0a1106d419551e32c227964
Jeniffer Callou <jenifferprc@gmail.com>
2022-01-01 22:05:50 +0100

    improved second solution

--- 04_largest_palindrome/004.rb
+++ 04_largest_palindrome/004.rb
@@ -1,6 +1,6 @@
 # Problem 04 - largest palindrom number.
 
-# 01- Solution
+# 01-Solution
 numbers = (100..999).to_a.product((100..999).to_a)
 products = numbers.map { |num| num[0] * num[1] }
 palindromes = []
@@ -8,27 +8,23 @@ palindromes = []
 products.each { |n| palindromes << n if n == n.to_s.reverse.to_i }
 puts palindromes.max
 
-# 02 - solution
-
-def find_largest_palindrome(list = [])
+# 02-Solution
+def find_largest_palindrome(list)
     list.max
-  end
+end
 
-def isPalindrome(list = [])
+def isPalindrome(list)
     palindrome_nums = []
-    list.each do |n|
-        if n == n.to_s.reverse.to_i
-            palindrome_nums << n
-        end    
-    end
+    list.each { |n| palindrome_nums << n if n == n.to_s.reverse.to_i }
+
     find_largest_palindrome(palindrome_nums)
 end
 
 def find_palindrome_numbers(min, max)
     numbers = (min..max).to_a.product((min..max).to_a)
     products = numbers.map { |num| num[0] * num[1] }
-    p numbers.count
+    
     isPalindrome(products)
 end
 
-p find_palindrome_numbers(100, 999)
+puts find_palindrome_numbers(100, 999)

082a0e17637afe93f651d9f8a300cc1d57f4492f
Jeniffer Callou <jenifferprc@gmail.com>
2022-01-01 21:18:41 +0100

    Adjusted names

--- 02_even_fibonacci/002.rb
+++ 02_even_fibonacci/002.rb
@@ -1,6 +1,4 @@
-# Problem 02 - Even Fibonacci Numbers
-# By considering the terms in the Fibonacci sequence whose values 
-# do not exceed four million, find the sum of the even-valued terms.
+# Problem 02 - find the sum of the even-valued terms.
 
 # 01-mySolution
 fibonacci_sequence = [1, 2]

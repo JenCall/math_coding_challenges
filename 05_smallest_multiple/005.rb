eb4ec53e050e35bea38f8cd542a460a1c0b82d73
Jeniffer Callou <jenifferprc@gmail.com>
2022-01-02 22:22:50 +0100

    add solution

--- 05_smallest_multiple/005.rb
+++ 05_smallest_multiple/005.rb
@@ -7,13 +7,13 @@
 
 # 01-Soultion - works with example but not efficient with 20.
 
-(230000000..240000000).each do |n|
+(1000..10000).each do |n|
     nums = []
-    (1..20).to_a.each { |d| nums << n if n % d == 0 }
-    puts n if nums.count == 20      
+    (1..10).to_a.each { |d| nums << n if n % d == 0 }
+    puts n if nums.count == 10      
 end
 
-# logic
+# 02-Optimized solution - logic
 def find_smallest_multiple(n)
 
 end

46635ca2415727be0eba7ba68c297cb479cdede4
Jeniffer Callou <jenifferprc@gmail.com>
2022-01-02 22:22:01 +0100

    add solution

--- 05_smallest_multiple/005.rb
+++ 05_smallest_multiple/005.rb
@@ -5,10 +5,15 @@
 # What is the smallest positive number that is evenly 
 # divisible by all of the numbers from 1 to 20?
 
-# 01-Soultion - works with example:
+# 01-Soultion - works with example but not efficient with 20.
 
-(1..10000).each do |n|
+(230000000..240000000).each do |n|
     nums = []
-    (1..10).to_a.each { |d| nums << n if n % d == 0 }
-    puts n if nums.count == 10      
+    (1..20).to_a.each { |d| nums << n if n % d == 0 }
+    puts n if nums.count == 20      
+end
+
+# logic
+def find_smallest_multiple(n)
+
 end

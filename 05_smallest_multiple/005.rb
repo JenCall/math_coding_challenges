9ab07b569b5128266c1246ba9eaf700bf4ac6bbf
Jeniffer Callou <jenifferprc@gmail.com>
2022-01-02 21:53:26 +0100

    add problem

---
+++ 05_smallest_multiple/005.rb
@@ -0,0 +1,12 @@
+# Problem 05
+
+# 2520 is the smallest number that can be divided by 
+# each of the numbers from 1 to 10 without any remainder.
+# What is the smallest positive number that is evenly 
+# divisible by all of the numbers from 1 to 20?
+
+divisible = (1..20).to_a
+
+(2520..10000).each do |n|
+    
+end

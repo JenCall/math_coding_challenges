93e2a1fda99ad40d2c18cdf0cae787ec5495c1a6
Jeniffer Callou <jenifferprc@gmail.com>
2022-01-03 22:23:29 +0100

    Optimized Solution

--- 06_sum_square_diff/006.rb
+++ 06_sum_square_diff/006.rb
@@ -2,12 +2,10 @@
 # Find the difference between the sum of the squares of the 
 # first one hundred natural numbers and the square of the sum.
 
-
+# 01 - My Solution
 def find_square(n)
     sum_squares = 0
-    (1..n).to_a.each do |n|
-        sum_squares += (n * n)
-    end
+    (1..n).to_a.each { |n| sum_squares += (n * n) }
     sum_squares
 end
 

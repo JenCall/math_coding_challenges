34bb54f7c6c3fed081948ba2bd56b1af93d13b9b
Jeniffer Callou <jenifferprc@gmail.com>
2022-01-03 22:10:44 +0100

    Add Problem

---
+++ 06_sum_square_diff/006.rb
@@ -0,0 +1,12 @@
+# Problem 06
+# Find the difference between the sum of the squares of the 
+# first one hundred natural numbers and the square of the sum.
+
+
+def find_square(n)
+    sum_squares = []
+    (1..n).to_a.each do |n|
+    end
+end
+
+p find_square(100)

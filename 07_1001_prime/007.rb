7420e3924d72728d5342a0526174ebf4cd0b6e25
Jeniffer Callou <jenifferprc@gmail.com>
2022-01-04 20:59:56 +0100

    Add first Solution

--- 07_1001_prime/007.rb
+++ 07_1001_prime/007.rb
@@ -6,8 +6,8 @@ require 'prime'
 
 def find_10001_Prime(n)
     prime_nums = []
-    Prime.each(200000) { |prime| prime_nums << prime }  
-    p prime_nums[n]
+    prime_nums = Prime.each(200000) { |prime| prime_nums << prime }  
+    prime_nums[n]
 end
 
-find_10001_Prime(10000)
+p find_10001_Prime(10000)

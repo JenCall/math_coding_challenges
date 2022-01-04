514cb6d9c4e819c8114fe75b20c0ec09294b3542
Jeniffer Callou <jenifferprc@gmail.com>
2022-01-04 20:58:18 +0100

    Add first Solution

--- 07_1001_prime/007.rb
+++ 07_1001_prime/007.rb
@@ -1,11 +1,12 @@
 # Problem 07
 
+# 01 - Solution
+
 require 'prime'
+
 def find_10001_Prime(n)
     prime_nums = []
-    Prime.each(200000) do |prime|    # if 5 dgs -> pf <= 2dgs, so 12dgs -> <= 5dgs
-        prime_nums << prime
-    end  
+    Prime.each(200000) { |prime| prime_nums << prime }  
     p prime_nums[n]
 end
 

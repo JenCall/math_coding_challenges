8d6116c9a24710bc2a6c3963904d07fced9dcf2c
Jeniffer Callou <jenifferprc@gmail.com>
2022-01-06 16:31:57 +0100

    add final better solution

--- 09_pytha_triplet/009.rb
+++ 09_pytha_triplet/009.rb
@@ -1,26 +1,22 @@
 # Problem 09
 
-# A Pythagorean triplet is a set of three natural numbers, a < b < c, for which, a2 + b2 = c2
-# For example, 32 + 42 = 9 + 16 = 25 = 52.
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc.
 
-def square_num(n)
-    square = (1..n).to_a.map { |n| n * n }
-    find_triple(square)
-end
+# 01 - Brute Force Solution
+def is_pythagorean_triplet?(a, b, c)
+    true if ((c * c)  == (a * a) + (b * b))
+  end
 
-def find_triple(list)
-    new_list = []
-    list.each_with_index do |n, index|
-        if list[index + 1] == nil 
-            break
-        elsif (n + list[index + 1]) == list[index + 2]
-            new_list << n
-            new_list << list[index + 1]
-            new_list << list[index + 2]
-        end
+def find_triple_sum(n)
+    (1..n).each do |a|
+        (a..n).each do |b|
+            c = (n - a - b)
+            if is_pythagorean_triplet?(a, b, c)
+                p (a * b * c)
+                break
+            end
+        end  
     end
-    new_list.each_slice(3).map { |e| e.inject(:+) }
 end
 
-p square_num(100)
+find_triple_sum(1000)

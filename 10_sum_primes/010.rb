2a5e03fcf0476dec325048631b33f4f40766aa3d
Jeniffer Callou <jenifferprc@gmail.com>
2022-01-07 17:35:04 +0100

    add solution

--- 10_sum_primes/010.rb
+++ 10_sum_primes/010.rb
@@ -1,3 +1,12 @@
 # Problem 10
 # Find the sum of primes below a certain number
 
+require 'prime'
+
+def find_sum_below(n)
+    prime_nums = []
+    prime_nums = Prime.each(n) { |prime| prime_nums << prime }.sum
+    prime_nums
+end
+
+p find_sum_below(2000000)

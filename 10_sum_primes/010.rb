cce99bf0a327565fb985ba6b2cebd7f82743d47f
Jeniffer Callou <jenifferprc@gmail.com>
2022-01-07 17:41:05 +0100

    solution improved

--- 10_sum_primes/010.rb
+++ 10_sum_primes/010.rb
@@ -3,10 +3,15 @@
 
 require 'prime'
 
+# 01 - first Solution with prime library
 def find_sum_below(n)
-    prime_nums = []
+    prime_nums = []  
     prime_nums = Prime.each(n) { |prime| prime_nums << prime }.sum
-    prime_nums
 end
-
 p find_sum_below(2000000)
+
+# 02 - second Solution - only for lower n
+def find_sum_below_1
+    (1..10).select {|x| (1..x).select{ |y| x % y == 0 }.size==2 }.sum
+end
+p find_sum_below_1

90938465f25909e9a6399e3224bc36f0e2fd6775
Jeniffer Callou <jenifferprc@gmail.com>
2022-01-10 21:59:53 +0100

    improved solution

--- 12_div_triangular/012.rb
+++ 12_div_triangular/012.rb
@@ -1,30 +1,25 @@
 # Problem 12
+# What is the value of the first triangle number to have over five hundred divisors?
 
-
-sum_previous_nums = Array.new
-(1..10).each do |n|
-    sum_previous_nums << (1..n).to_a.reduce(0, :+)
+def sum_previous_nums
+    sum_previous_nums = Array.new
+    (1..10).each { |n| sum_previous_nums << (1..n).to_a.reduce(0, :+) }
+    over_nhundred_divisors(sum_previous_nums)
 end
 
-result = Hash.new
-sum_previous_nums.each do |n|
-    array = []
-    (1..n).each do |i|
-        array << i if n % i == 0 
-        if (array.count > 6)
-            result[n] = array
-            break
+def over_nhundred_divisors(list)
+    result = Hash.new
+    list.each do |n|
+        array = []
+        (1..n).each do |i|
+            array << i if n % i == 0 
+            if (array.count > 6)
+                result[n] = array
+                break
+            end
         end
     end
+    puts result
 end
-puts result
 
-# rersult_1 = result.values
-
-# puts "start calculating third part"
-# rersult_1.each_with_index do |array, index|
-#     if array.count == 200
-#     p index
-#     break
-#     end
-# end
+p sum_previous_nums

ecf9b79177315d91b8c09b31eebcfd89c139a518
Jeniffer Callou <jenifferprc@gmail.com>
2022-01-14 21:03:08 +0100

    add solution

--- 15_lattice_paths/015.rb
+++ 15_lattice_paths/015.rb
@@ -4,5 +4,17 @@
 def find_paths
     puts (1..40).inject(:*) / (1..20).inject(:*) ** 2
 end    
-
 find_paths
+
+
+# Combinatorial Solution 
+# much faster O(n) solution
+def cout_paths(n)
+    result = 1
+
+    (1..n).to_a.each do |i|
+        result << (result * (n + i) / i)
+    end
+    result
+end 
+cout_paths(2)

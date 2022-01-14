b16de2a67d8e86c92f3ee3e5ce8e4e43473a7462
Jeniffer Callou <jenifferprc@gmail.com>
2022-01-14 21:18:05 +0100

    add solution

--- 15_lattice_paths/015.rb
+++ 15_lattice_paths/015.rb
@@ -7,14 +7,17 @@ end
 find_paths
 
 
-# Combinatorial Solution 
-# much faster O(n) solution
-def cout_paths(n)
-    result = 1
 
-    (1..n).to_a.each do |i|
-        result << (result * (n + i) / i)
-    end
-    result
-end 
-cout_paths(2)
+
+# https://betterexplained.com/articles/navigate-a-grid-using-combinations-and-permutations/
+
+# # NOT Working yet - Combinatorial Solution 
+# # much faster O(n) solution
+# def cout_paths(n)
+#     result = 1
+#     (1..n).to_a.each do |i|
+#         result += (result * (n + i) / i)
+#     end
+#     p result / 20
+# end 
+# cout_paths(20)

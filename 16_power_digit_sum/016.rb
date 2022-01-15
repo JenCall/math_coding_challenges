e9742dd4131ce7499d2193a6b7485eab58ca2afc
Jeniffer Callou <jenifferprc@gmail.com>
2022-01-15 16:56:40 +0100

    add solution

--- 16_power_digit_sum/016.rb
+++ 16_power_digit_sum/016.rb
@@ -1,7 +1,10 @@
 # Problem 16
 # Power digit sum
 
+# 01 - Solution
+
 def sum_power_digits(num, power)
     puts (num ** power).to_s.split("").map { |n| n.to_i }.sum
 end
+
 sum_power_digits(2, 1000)

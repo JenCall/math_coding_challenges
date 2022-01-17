4779c918627db69bc87c8a177ee488e4af758180
Jeniffer Callou <jenifferprc@gmail.com>
2022-01-17 20:49:57 +0100

    add partial solution

--- 20_factorial_sum/020.rb
+++ 20_factorial_sum/020.rb
@@ -1,5 +1,19 @@
 # Problem 20
 # Find the sum or the 100! digits
 
-def
+def find_factorial(num)
+    sum = 1
+    i = 2
+    while i <= num
+        sum = sum * i
+        i += 1
+    end
+    sum_digits(sum)
 end
+
+def sum_digits(num)
+    
+end
+
+
+find_factorial(10)

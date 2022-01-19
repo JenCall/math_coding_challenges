c51cd9455b6f12d808e773e1006a14b6957fd5ca
Jeniffer Callou <jenifferprc@gmail.com>
2022-01-19 20:48:29 +0100

    add partial solution

--- 21_amicable_nums/021.rb
+++ 21_amicable_nums/021.rb
@@ -20,12 +20,34 @@ def comparision(values)
     # ( invert[k] == values[k] && ? invert[k] : k )
     (invert).each do |keys_1, values_1| 
         (values).each do |keys_2, values_2| 
-        if (keys_1 == keys_2 && values_1 == values_2)
-            new_hash[keys_1] = values_1
-        end
+            new_hash[keys_1] = values_1 if (keys_1 == keys_2 && values_1 == values_2 && keys_1 != values_1)
         end
     end
     p new_hash
 end
 
-find_divisors(11000)
+find_divisors(10000)
+
+# add other solution
+
+def proper_divisor(n)
+    res = [1]
+    i = 2
+    while i * i <= n
+      if n % i == 0
+        res << i
+        if n / i != i
+          res << (n / i)
+        end
+      end
+      i += 1
+    end
+    return res
+  end
+  
+  def amicable_number?(n)
+    sum = proper_divisor(n).inject(:+)
+    return (n != sum) && (n == proper_divisor(sum).inject(:+))
+  end
+  
+  puts (1..10000).select {|i| amicable_number?(i) }.inject(:+)

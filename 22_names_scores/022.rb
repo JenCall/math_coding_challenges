ce36f540309c0d0eaa2b863bc848010d97024c6c
Jeniffer Callou <jenifferprc@gmail.com>
2022-01-20 21:45:02 +0100

    add partial solution

--- 22_names_scores/022.rb
+++ 22_names_scores/022.rb
@@ -1,5 +1,6 @@
 # Problem 22
 
+#first idea
 input = File.open("names.txt", 'r')
 data = input.gets.scan(/\w+/).sort
 teste = ["COLIN"]
@@ -21,6 +22,43 @@ teste.each do |name|
         # if letter != nil
         #     sum += dictionary[letter]
         # end
-
     end
 end
+
+# correct solution
+
+def alpha_score_for_name(name, alpha_values)
+    score = 0
+    name.split("").each do |alphabet|
+      score += alpha_values[alphabet]
+    end
+    return score
+  end
+  
+  names = []
+  names_string = ""
+  File.open("names.txt").each do |line|
+   names_string = line
+  end
+  names_string.split(",").each do |name|
+    names << name.sub("\"", "").sub("\"", "")
+  end
+  names.sort!
+  
+  # generate the alphabetical values hash
+  
+  alpha_values = {}
+  value = 1
+  ('A'..'Z').each do |alphabet|
+    alpha_values[alphabet] = value
+    value += 1
+  end
+  
+  sum_of_scores = 0
+  position = 1
+  names.each do |name|
+    sum_of_scores += position * alpha_score_for_name(name, alpha_values)
+    position += 1
+  end
+  
+  puts "Answer: #{sum_of_scores}"

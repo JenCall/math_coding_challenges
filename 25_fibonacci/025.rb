e38c19d4a703168f2cb6577c8178bea5167c2f73
Jeniffer Callou <jenifferprc@gmail.com>
2022-01-22 11:04:36 +0100

    add partial solution

--- 25_fibonacci/025.rb
+++ 25_fibonacci/025.rb
@@ -4,7 +4,15 @@
 def find_fibonacci(n)
     num_1 = 1
     num_2 = 2
-    fibonacci = []
-    number 
-
+    fibonacci = [1, 1]
+    i = 1
+    while fibonacci[i] < n
+        num = fibonacci[-2] + fibonacci[-1]
+        fibonacci << num
+        i += 1
+    end
+    puts fibonacci
+    puts "finish"
 end
+
+find_fibonacci(12)

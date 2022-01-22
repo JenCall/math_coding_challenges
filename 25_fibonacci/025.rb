4cd75ce9a72f2f66e0e1be42f1a97d4bfa040866
Jeniffer Callou <jenifferprc@gmail.com>
2022-01-22 11:08:26 +0100

    add partial solution

--- 25_fibonacci/025.rb
+++ 25_fibonacci/025.rb
@@ -2,16 +2,14 @@
 # Find fibonacci number with 1000 digits
 
 def find_fibonacci(n)
-    num_1 = 1
-    num_2 = 2
     fibonacci = [1, 1]
-    i = 1
-    while fibonacci[i] < n
+    i = 2
+    while i < n
         num = fibonacci[-2] + fibonacci[-1]
         fibonacci << num
         i += 1
     end
-    puts fibonacci
+    p fibonacci
     puts "finish"
 end
 

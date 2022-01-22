0c37e48f8e3dcac3972d9880416e23542b7c7e14
Jeniffer Callou <jenifferprc@gmail.com>
2022-01-22 11:20:04 +0100

    add solution

--- 25_fibonacci/025.rb
+++ 25_fibonacci/025.rb
@@ -8,9 +8,16 @@ def find_fibonacci(n)
         fibonacci << fibonacci[-2] + fibonacci[-1]
         i += 1
     end
-    p fibonacci
+    find_1000_digits(fibonacci)
 end
 
+def find_1000_digits(list)
+    list.each do |n|
+        if (n.to_s.split("").count == 1000)
+            puts "Answer: #{list.find_index(n) + 1}"
+            break
+        end
+    end
+end
 
-
-find_fibonacci(100)
+find_fibonacci(10000)

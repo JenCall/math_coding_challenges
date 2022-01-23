73ecc9bdc850fd98db9dbcfbf2ae543a16f411a1
Jeniffer Callou <jenifferprc@gmail.com>
2022-01-23 20:15:19 +0100

    add partial solution

--- 26_reciprocal_cycles/026.rb
+++ 26_reciprocal_cycles/026.rb
@@ -2,6 +2,15 @@
 # Find the longest recurring cycle in its decimal fraction part.
 
 def find_decimal(n)
+    decimals = []
+    (2..n).each do |n|
+        num = (1 / n.to_f)
+        decimals << num
+    end
+    p decimals
+end
+
+def
 end
 
 find_decimal(10)

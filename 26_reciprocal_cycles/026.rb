b6c5a022243c34b4f4f15b6f2a9d39803385869e
Jeniffer Callou <jenifferprc@gmail.com>
2022-01-23 20:43:15 +0100

    add improved solution

--- 26_reciprocal_cycles/026.rb
+++ 26_reciprocal_cycles/026.rb
@@ -17,7 +17,7 @@ end
 
 find_decimal(1000)
 
-# improved solution from the internet
+# improved solution from net
 puts (0..1000).map { |d| 
   (1..d).detect(lambda{0}) { |t| (10**t % d) == 1 } 
 }.each_with_index.max[1]

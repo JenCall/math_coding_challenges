fce83de8a28dd934e5e3ae012160aef2a2fd29af
Jeniffer Callou <jenifferprc@gmail.com>
2022-01-23 20:25:04 +0100

    add partial solution

--- 26_reciprocal_cycles/026.rb
+++ 26_reciprocal_cycles/026.rb
@@ -7,10 +7,16 @@ def find_decimal(n)
         num = (1 / n.to_f)
         decimals << num
     end
-    p decimals
+    find_recurring_cycle(decimals)
 end
 
-def
+def find_recurring_cycle(decimals)
+    recurring_nums = []
+    decimals.each do |n|
+        recurring_nums << n.to_s.split("").uniq.count - 2
+    end
+    i = recurring_nums.max
+    p recurring_nums.find_index(i) + 2
 end
 
 find_decimal(10)

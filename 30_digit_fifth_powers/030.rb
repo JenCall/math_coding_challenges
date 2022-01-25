72d5d487c6571ed9e3eeb225a337b584d643a4e3
Jeniffer Callou <jenifferprc@gmail.com>
2022-01-25 19:42:18 +0100

    add one line solution

--- 30_digit_fifth_powers/030.rb
+++ 30_digit_fifth_powers/030.rb
@@ -1,6 +1,7 @@
 # Problem 30
 # Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
 
+# My first solution
 def form_array(n)
     integers = (1000..n).to_a
     sum_fifth_power(integers)
@@ -15,5 +16,10 @@ def sum_fifth_power(list)
     end
     puts "Answer: #{fifth_powers.sum}"
 end
-
 form_array(500000)
+
+# shorter solution
+def sum_fifth_power_2(n)
+    p (2..n).select { |i| i == i.to_s.each_char.map { |d| d.to_i**5 }.reduce(:+) }.reduce(:+)
+end
+sum_fifth_power_2(500000)

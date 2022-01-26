57847f28101e8d8115b0b7d4de89f120860e360b
Jeniffer Callou <jenifferprc@gmail.com>
2022-01-26 19:30:39 +0100

    add comment

--- 34_digit_factorials/034.rb
+++ 34_digit_factorials/034.rb
@@ -4,20 +4,19 @@
 # My first Solution
 def find_factorial(num)  
     factorials = 0
-    
-    (144..num).each do |n|
+
+    (144..num).each { |n|
         array_of_nums = n.to_s.split("").map { |i| i.to_i }
         sum = 0
         array_of_nums.each { |j| sum += (1..j).inject(:*) || 1 }
         factorials += sum if sum == n 
-    end
+    }
+
     puts "Answer: #{factorials}"
 end
 find_factorial(50000)
 
 # better solution found
-puts (0..50000).select { |i|
-  i.to_s.length > 1 && i == i.to_s.each_char.map { |d| (1..d.to_i).reduce(1, :*) }.reduce(:+)
-}.reduce(:+)
+puts (0..50000).select { |i| i.to_s.length > 1 && i == i.to_s.each_char.map { |d| (1..d.to_i).reduce(1, :*) }.reduce(:+) }.reduce(:+)
 
 # take away: inject

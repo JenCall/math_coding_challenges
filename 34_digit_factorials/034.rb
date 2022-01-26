81fe7d5d765319b4de89478130e7399e91d8de11
Jeniffer Callou <jenifferprc@gmail.com>
2022-01-26 18:56:28 +0100

    add partial solution

--- 34_digit_factorials/034.rb
+++ 34_digit_factorials/034.rb
@@ -1,5 +1,19 @@
 # Problem 34
 # Find the sum of all numbers which are equal to the sum of the factorial of their digits.
 
-def 
+def find_factorial(num)
+    sum = 1
+    i = 2
+    factorials = Hash.new
+    (3..num).each do |n|
+        nums = n
+        while i <= n
+            sum = sum * i
+            i += 1
+        end
+        factorials[n] = sum
+        p factorials
+    end
 end
+
+find_factorial(145)

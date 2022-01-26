7be5f7cc04084e4eda4ed9aec440589eddc51c5a
Jeniffer Callou <jenifferprc@gmail.com>
2022-01-26 19:28:12 +0100

    add solution-improved

--- 34_digit_factorials/034.rb
+++ 34_digit_factorials/034.rb
@@ -1,24 +1,21 @@
 # Problem 34
 # Find the sum of all numbers which are equal to the sum of the factorial of their digits.
 
+# My first Solution
 def find_factorial(num)  
     factorials = 0
     
     (144..num).each do |n|
-        num = n.to_s.split("").map { |i| i.to_i }
+        array_of_nums = n.to_s.split("").map { |i| i.to_i }
         sum = 0
-        num.each do |j|
-            sum += (1..j).inject(:*) || 1
-        end
-        if sum == n 
-            factorials += sum
-        end
+        array_of_nums.each { |j| sum += (1..j).inject(:*) || 1 }
+        factorials += sum if sum == n 
     end
-    p factorials
+    puts "Answer: #{factorials}"
 end
-
 find_factorial(50000)
 
+# better solution found
 puts (0..50000).select { |i|
   i.to_s.length > 1 && i == i.to_s.each_char.map { |d| (1..d.to_i).reduce(1, :*) }.reduce(:+)
 }.reduce(:+)

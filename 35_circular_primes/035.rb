0479770ee84bf46f66f3dc818b5d3688e2127aa7
Jeniffer Callou <jenifferprc@gmail.com>
2022-01-27 21:36:55 +0100

    add partial solution

--- 35_circular_primes/035.rb
+++ 35_circular_primes/035.rb
@@ -1,5 +1,23 @@
 # Problem 35
 # How many circular primes are there below one million?
 
-def
+require 'prime'
+
+def find_rotations(n)
+    nums = n.to_s.chars
+    nums.map { nums.rotate.join.to_i }
 end
+
+
+# total = Prime.each(1000000).select do |n|
+#     find_rotations(n) {|d| Prime.prime?(d) }
+# end
+# p total.count
+  
+
+# require 'math'
+puts (1..1000000).select { |i|
+  (1..i.to_s.length).all? { |j|
+    i.to_s.split('').rotate(j).join('').to_i.prime?
+  }
+}.count

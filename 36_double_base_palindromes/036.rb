eb8b409bfd4ba5046d8d8d12ad0fdd65f048f98e
Jeniffer Callou <jenifferprc@gmail.com>
2022-01-31 20:56:26 +0100

    add solution

--- 36_double_base_palindromes/036.rb
+++ 36_double_base_palindromes/036.rb
@@ -2,5 +2,16 @@
 # Find the sum of all numbers, less than one million, 
 # which are palindromic in base 10 and base 2.
 
-def find_palindrome
+def find_palindrome(n)
+    sum = 0
+    (1..n).each do |n|
+        binary = n.to_s(2)
+        
+        if (n.to_s == n.to_s.reverse) && (binary == binary.reverse)
+            sum += n
+        end
+    end
+    p sum
 end
+
+find_palindrome(1000000)

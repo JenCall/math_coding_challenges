95d8737d4de350a10b53ad9a790dc6c0b1842658
Jeniffer Callou <jenifferprc@gmail.com>
2022-01-31 20:57:36 +0100

    add solution improved

--- 36_double_base_palindromes/036.rb
+++ 36_double_base_palindromes/036.rb
@@ -6,12 +6,10 @@ def find_palindrome(n)
     sum = 0
     (1..n).each do |n|
         binary = n.to_s(2)
-        
-        if (n.to_s == n.to_s.reverse) && (binary == binary.reverse)
-            sum += n
-        end
+
+        (sum += n) if (n.to_s == n.to_s.reverse) && (binary == binary.reverse)
     end
-    p sum
+    puts "Answer: #{sum}"
 end
 
 find_palindrome(1000000)

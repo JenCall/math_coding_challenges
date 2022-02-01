b903c3e147725856d08c6ef496557af43cdbe114
Jeniffer Callou <jenifferprc@gmail.com>
2022-02-01 21:11:43 +0100

    add solution for the example

--- 48_self_powers/048.rb
+++ 48_self_powers/048.rb
@@ -1,5 +1,12 @@
 # Problem 48
 # Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
 
-def  
+def sum_self_powers(n)
+    sum = 0
+    (1..n).each do |i|
+        sum += i ** i
+    end
+    puts "Answer: #{sum.to_s[-10...-1]}"
 end
+
+sum_self_powers(10)

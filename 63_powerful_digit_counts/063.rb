580f745d893bdc2b400ffcc83b628767d39a4311
Jeniffer Callou <jenifferprc@gmail.com>
2022-02-02 21:36:45 +0100

    add partial solution

--- 63_powerful_digit_counts/063.rb
+++ 63_powerful_digit_counts/063.rb
@@ -1,5 +1,15 @@
 # Problem 63
 # How many n-digit positive integers exist which are also an nth power?
 
-def 
+def find_power_counts(max)
+    digits = []
+    (1..max).each do |n|
+        (1..max).each do |i|
+            num = n ** i
+            digits << num if num.to_s.split("").count == i
+        end
+    end
+    p digits.sum
 end
+
+find_power_counts(100)
